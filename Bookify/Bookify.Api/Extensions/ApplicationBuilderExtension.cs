5fc2533a9c97e0b888c69ac9dd68a0e197331ccf
Eduardo Medina <Eduardo.Medina@vanco.com>
2024-02-15 11:47:18 -0600

    adds: global error middleware

--- Bookify/Bookify.Api/Extensions/ApplicationBuilderExtension.cs
+++ Bookify/Bookify.Api/Extensions/ApplicationBuilderExtension.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Infrastructure;
+﻿using Bookify.Api.Middleware;
+using Bookify.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Api.Extensions;
@@ -12,5 +13,10 @@ public static class ApplicationBuilderExtension
         using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         dbContext.Database.Migrate();
+    }
+
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
     }
 }
