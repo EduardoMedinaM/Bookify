98371b3ad83b3d1cdd10b9e8560437d35a6d5933
Eduardo Medina <Eduardo.Medina@vanco.com>
2024-03-27 17:24:15 -0600

    feat: health checks

--- Bookify/Bookify.Api/Program.cs
+++ Bookify/Bookify.Api/Program.cs
@@ -1,6 +1,8 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
 using Bookify.Infrastructure;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,10 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+
+//builder.Services
+//    .AddHealthChecks()
+//    .AddCheck<CustomSqlHealthCheck>("custom-sql");
 
 var app = builder.Build();
 
@@ -43,4 +49,31 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapHealthChecks("health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 app.Run();
+
+/*
+ * In case you ned a custom health check...
+ * It's not recommendable at all
+    public class CustomSqlHealthCheck(ISqlConnectionFactory sqlConnectionFactory) : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var connection = sqlConnectionFactory.CreateConnection();
+                await connection.ExecuteScalarAsync("SELECT 1");
+
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy(exception: ex);
+            }
+        }
+    }
+ */
