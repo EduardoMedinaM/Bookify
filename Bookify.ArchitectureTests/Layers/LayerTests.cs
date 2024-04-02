9ac0e4d6860b7c142335f0ebe9bc7a9170d22d64
Eduardo Medina <Eduardo.Medina@vanco.com>
2024-04-02 17:56:36 -0600

    fixes: Validators are now internal

---
+++ Bookify.ArchitectureTests/Layers/LayerTests.cs
@@ -0,0 +1,63 @@
+﻿using Bookify.ArchitectureTests.Infrastructure;
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Bookify.ArchitectureTests.Layers;
+
+public class LayerTests : BaseTest
+{
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOn_ApplicationLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void InfrastructureLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
