ab9e09b7554527e1ca4f9be5f4372359436ac13a
Eduardo Medina <Eduardo.Medina@vanco.com>
2024-04-02 17:21:28 -0600

    feat: functional tests

--- Bookify/Bookify.Infrastructure/Authentication/AuthenticationOptions.cs
+++ Bookify/Bookify.Infrastructure/Authentication/AuthenticationOptions.cs
@@ -3,8 +3,8 @@
     public sealed class AuthenticationOptions
     {
         public string Audience { get; init; } = string.Empty;
-        public string MetadataUrl { get; init; } = string.Empty;
+        public string MetadataUrl { get; set; } = string.Empty;
         public bool RequireHttpsMetadata { get; init; }
-        public string Issuer { get; init; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
     }
 }
