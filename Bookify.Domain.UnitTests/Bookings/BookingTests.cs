b047ef086e21e756fb8f370446400dc081499063
Eduardo Medina <Eduardo.Medina@vanco.com>
2024-04-02 14:39:46 -0600

    feat: domain unit tests

---
+++ Bookify.Domain.UnitTests/Bookings/BookingTests.cs
@@ -0,0 +1,32 @@
+﻿using Bookify.Domain.Bookings;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Shared;
+using Bookify.Domain.UnitTests.Apartments;
+using Bookify.Domain.UnitTests.Infrastructure;
+using Bookify.Domain.UnitTests.Users;
+using Bookify.Domain.Users;
+using FluentAssertions;
+
+namespace Bookify.Domain.UnitTests.Bookings;
+
+public class BookingTests : BaseTest
+{
+    [Fact]
+    public void Reserve_Should_RaiseBookingReservedDomainEvent()
+    {
+        // Arrange
+        var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+        var price = new Money(10.0m, Currency.Usd);
+        var duration = DateRange.Create(new DateOnly(2024, 1, 1), new DateOnly(2024, 1, 10));
+        var apartment = ApartmentData.Create(price);
+        var pricingService = new PricingService();
+
+        // Act
+        var booking = Booking.Reserve(apartment, user.Id, duration, DateTime.UtcNow, pricingService);
+
+        // Assert
+        var bookingReservedDomainEvent = AssertDomainEventWasPublished<BookingReservedDomainEvent>(booking);
+
+        bookingReservedDomainEvent.BookingId.Should().Be(booking.Id);
+    }
+}
